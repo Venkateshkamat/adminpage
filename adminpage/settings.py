9301dbb816b71804b39dd18c25b0ea63dbca7b6c
Venkateshkamat <70506306+Venkateshkamat@users.noreply.github.com>
2021-06-15 20:41:02 +0530

    Removed django key :|

--- adminpage/settings.py
+++ adminpage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z9r&rv18&po%wno7x9u&a_i#o7p7a31ha$-5r27(p=w(!(dmq-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
